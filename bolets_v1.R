5052bcdc544330768582d4ad7dd17f1d02e722d9
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-05-08 12:39:24 +0200

    commit

--- bolets_v1.R
+++ bolets_v1.R
@@ -5,16 +5,48 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 
-# llegir un shape
+
+# ----- DADES SHAPE -----
+# -----------------------
+
 
 comarques_punts <- st_read("data/raw/COMARQUES_punts.shp")
 
+# ----- FUNCIONS en ALTRE ARXIU -------
+# ------------------------------------
 
-#   ---- SUMARIZE ----
-#   ------------------
+source("scripts/funcions.R")
 
-#   AGRUPO per COMARQUES
-#   CALCULO 
-#      .-) la MITJA de LONGITUDS
-#      .-) la SUMA de LONGITUDS x COMARCA
-#      .-) El NÚMERO de FILES x selecció =  n()
+
+#   ---------- OBJECTIU DE LA APP ---------
+#   ---------------------------------------
+
+#     -) Crear una APP que ajudi a detectar zones idonees on hi hagi bolets
+#     -) He de saber quines variables fisiques son idonees pels bolets
+#     -) Començaré amb DADES CLIMÀTIQUES
+
+#     DADES CLIMÀTIQUES:
+
+#     -) Usare la API openmeteo per treure dades de
+#     -) Venen de la API OPEN METEO
+
+#     -) He creat FUNCIONS dins de ARXIU funcionR que obté dades
+#     -) Les dades son per CADA COORDENADA
+#     -) Les dades son TEMP, HUMITAT i VENT
+#     -) De cada una tenim Max i MIN
+
+#     DADES COMARCA
+
+#     -) Per cada COMARCA tinc un shape de punts (comarques_punts)
+#     -) Calcularé per cada COMARCA la mitja de tots el punts
+#     -) Així sabré per COMARCA la MITJA de TEMP, HUMIT, VENT
+
+#     EM FALTARIA DADES de:
+
+#     -) Obtenir la PLUJA
+#     -) La ALTITUD
+#     -) El tipus de VEGETACIÓP
+
+
+
+
